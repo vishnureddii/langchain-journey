34c1e86029c33fb1b528cc2cc574af0fa2ce2acb
vishnureddii <106023409+vishnureddii@users.noreply.github.com>
2026-05-29 01:29:31 +0530

    Update llm_demo.py

--- 01_fundamentals/01_LLM_models/llm_demo.py
+++ 01_fundamentals/01_LLM_models/llm_demo.py
@@ -1,10 +1,7 @@
 from langchain_google_genai import GoogleGenerativeAI
-
 from dotenv import load_dotenv
 
 load_dotenv()
-
 llm = GoogleGenerativeAI(model = "gemini-2.5-flash-lite")
 result = llm.invoke("what is the capital of Delhi?")
-
 print(result)
